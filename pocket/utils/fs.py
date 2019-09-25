a23327d56919ed0050c0e2099f3c23ed4135a20d
Shubham Sharma <shubhamsharma1172@gmail.com>
2019-09-25 16:34:56 +0530

    create container

--- pocket/utils/fs.py
+++ pocket/utils/fs.py
@@ -1,6 +1,9 @@
 import os
+import shutil
+import tarfile
+from pocket.core.pull import Pull
 
-BASE_PATH = os.path.join(os.environ['HOME'], '.pocket')
+BASE_PATH = os.path.join('/opt', 'pocket')
 
 
 def get_path_to_images():
@@ -21,3 +24,24 @@ def get_path_to_manifest_file(manifest_name):
 
 def get_path_to_container(container_id):
     return os.path.join(BASE_PATH, 'containers', container_id)
+
+
+def _extract(source, dest):
+    for tar in os.listdir(source):
+        with tarfile.open(os.path.join(source, tar), 'r') as layer_tarfile:
+            layer_tarfile.extractall(dest)
+
+
+def setup_fs(image, container_id):
+    path_to_container = get_path_to_container(container_id)
+    os.makedirs(path_to_container)
+    if not os.path.isdir(get_path_to_manifest(image)):
+        Pull(image).run()
+    _extract(get_path_to_layers(image), path_to_container)
+    # TODO: mount stuff
+    pass
+
+
+def clean_fs(container_id):
+    path_to_container = get_path_to_container(container_id)
+    shutil.rmtree(path_to_container)
